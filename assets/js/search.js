eb0d982c615c845242afd9837a7ef998395f8d87
Pawel Osmolski <pawel@pawel-osmolski.com>
2023-05-19 12:58:32 -0500

    Search change to 'on' method for binding events

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-    $("#search-textarea").keypress(function (e) {
-        if(e.which === 13 && !e.shiftKey) {
+    $("#search-textarea").on("keypress", function (e) {
+        if (e.which === 13 && !e.shiftKey) {
             e.preventDefault();
-        
             $(this).closest("form").submit();
         }
     });
